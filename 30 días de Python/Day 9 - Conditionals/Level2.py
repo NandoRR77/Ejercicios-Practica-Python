92a6f2d0d2716963d0e428a309d89756e1c3a956
NandoRR77 <nandorrea77@gmail.com>
2025-10-15 09:48:52 -0500

    15.10.2025_Mac

--- 30 días de Python/Day 9 - Conditionals/Level2.py
+++ 30 días de Python/Day 9 - Conditionals/Level2.py
@@ -26,11 +26,98 @@ If a fruit doesn't exist in the list add the fruit to the list and print the mod
 If the fruit exists print('That fruit already exist in the list')
 '''
 
-#1. Create an empty dictionary called dog
-print('# 1. Create an empty dictionary called dog')
 
-dog = {}
+# 1. Write a code which gives grade to students according to theirs scores
+print('# 1. Write a code which gives grade to students according to theirs scores')
 
-print(f'Variable dog is type {type(dog)}')
+note = int(input('Ingrese su nota ==> '))
+
+'''
+# Con condicionales
+if note < 0 or note > 100:
+    print(f'La nota ingresada no es válida')
+elif note <= 49:
+    print(f'Para la nota {note} la calificación es F')
+elif note <= 59:
+    print(f'Para la nota {note} la calificación es D')
+elif note <= 69:
+    print(f'Para la nota {note} la calificación es C')
+elif note <= 89:
+    print(f'Para la nota {note} la calificación es B')
+else:
+    print(f'Para la nota {note} la calificación es A')
+'''    
+    
+#Con función
+#Defino listas con los rangos usando list comprenhesion
+A = [i for i in range(90,101)]
+B = [i for i in range(70,90)]
+C = [i for i in range(60,70)]
+D = [i for i in range(50,60)]
+F = [i for i in range(50)]
+
+#Defino el diccionario con la calificación y las notas
+grades = {
+    'Grade A': A,
+    'Grade B': B,
+    'Grade C': C,
+    'Grade D': D,
+    'Grade F': F, 
+}
+
+def calcular_calificacion(note):
+    for calificacion, notas in grades.items():
+        if note in notas:
+            return f'Para la nota {note}, la calificacion es {calificacion}'
+    return 'Nota no valida'
+
+print(calcular_calificacion(note))
+
+print('****************************************************\n')
+
+
+# 2. Check if the season is Autumn, Winter, Spring or Summer
+print('2. Check if the season is Autumn, Winter, Spring or Summer')
+
+#Defino un diccionario con los meses y las estaciones
+estaciones = {
+    'Autumn':['September', 'October', 'November'],
+    'Winter':['December', 'January', 'February'],
+    'Spring':['March', 'April', 'May'],
+    'Summer':['June', 'July', 'August'],
+}
+
+#Defino lista de meses para que solo seleccionen un válido
+meses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+mes_buscado = input(f'Ingrese un mes de la siguiente lista \n =>{meses}\n')
+
+
+def valida_estacion(mes_buscado):
+    for estacion, meses in estaciones.items():
+        if mes_buscado in meses:
+            print(f'La estación que corresponde según el mes ingresado es => {estacion}')
+    print(f'El mes {mes_buscado} no es válido')
+
+valida_estacion(mes_buscado)
+          
+print('****************************************************\n')
+
+
+
+# 3. Validate fruits
+print(' 3. Validate fruits')
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruit = input(f'Ingrese una fruta =>  ')
+
+def valida_fruta(fruit):
+    if fruit in fruits:
+        return f'That fruit already exist in the list {fruits}'
+    else:
+        fruits.append(fruit)
+    return f'The friuit {fruit} was added to list {fruits}' 
+
+print(valida_fruta(fruit))
 
 print('****************************************************\n')
--- Python_Platzi/1. Fundamentos de Python/26 Diccionarios.py
+++ Python_Platzi/1. Fundamentos de Python/26 Diccionarios.py
@@ -11,9 +11,9 @@ my_dict = {
 print(my_dict)
 print(len(my_dict)) #conocer la cantidad de elementos que tiene el diccionario
 
-print(my_dict['name']) #Leer una valor ingresando la llave 'name'
+print(my_dict['name']) #Leer una valor ingresando la clave 'name'
 print(my_dict.get('age')) #otra forma de leer un valor con el método .get()
-print(my_dict.get('escolaridad')) #si no existe la llave, arroja None y se puede manejar el error
+print(my_dict.get('escolaridad')) #si no existe la clave, arroja None y se puede manejar el error
 
 print('avión' in my_dict) #conocer si una llave está en un diccionario, True or False
 print('direccion' in my_dict) # si no está, arroja false
